6432a7013f3d7396e57a13b3d3faab18f167473b
phil <p@pkpowell.net>
2025-01-02 16:27:45 +0100

    updates

--- build.sh
+++ build.sh
@@ -1,18 +1,37 @@
 #!/bin/bash
 
+branch = "main"
+os  = "darwin"
+
+while getopts "b:a:" flag; do
+ case $flag in
+   b) # Handle the -b flag
+   branch = $OPTARG
+   ;;
+   a) # Handle the -a flag
+   os = $OPTARG
+   ;;
+   \?)
+   echo "Invalid option: $OPTARG" 1>&2
+   ;;
+ esac
+done
+
 if [ ! -d "./tailscale" ]; then
     git clone https://github.com/tailscale/tailscale.git tailscale
 fi
 
 cd ./tailscale
 
-if [ $1 ]; then
-    echo "switching to branch $1"
-    git switch $1
-else
-    git switch main
-fi
+git switch $branch
 
+# if [ $1 ]; then
+#     echo "switching to branch $1"
+#     git switch $1
+# else
+#     git switch main
+# fi
+echo "pulling latest changes from $branch"
 git pull
 
 sh ./build_dist.sh tailscale.com/cmd/tailscaled && {
